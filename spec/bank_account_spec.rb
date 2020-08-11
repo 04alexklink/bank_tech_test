111a9718b0383f5e996ba88bff6d712cc6d9b999
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-08-11 11:03:45 +0100

    Refactored bank_account_spec to make use of let statement for a new bankaccount. DRY tests

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,41 +1,36 @@
 require '/bank_account'
 describe BankAccount do 
+  let(:bankaccount) { BankAccount.new }
+
   context 'initialisation of a new bank account' do
   it 'should be created with a starting balance of 0' do
-    bankaccount = BankAccount.new
     expect(bankaccount.balance).to eq(0)
   end
   it 'should have a transaction history of an array containing 1 nested array of ["date", "credit", "debit", "balance"]' do
-    bankaccount = BankAccount.new
     expect(bankaccount.transactions[0]).to eq(["date", "credit", "debit", "balance"])
   end
 end
   context 'deposit into account' do
   it 'bank account owner should make a deposit of £50 and see balance increase to £50' do
-    bankaccount = BankAccount.new
     bankaccount.deposit(50)
     expect(bankaccount.balance).to eq(50)
   end
   it 'bank account owner should make a deposit of £50 and add relevant transaction info to transactions array' do
-    bankaccount = BankAccount.new
     bankaccount.deposit(50)
     expect(bankaccount.transactions[1]).to eq([Time.new.strftime('%d/%m/%Y'), "0", "50", "50"])
   end
 end
   context 'withdrawal' do
   it 'bank account owner should make a withdrawal of £50 and see balance decrease to £50' do
-    bankaccount = BankAccount.new
     bankaccount.deposit(50)
     bankaccount.withdraw(50)
     expect(bankaccount.balance).to eq(0)
   end
   it 'bank account owner can only make a withdrawal if withdrawal amount < balance' do
-    bankaccount = BankAccount.new
     expect { bankaccount.withdraw(50) }.to raise_error("Your withdraw request is greater than your account balance. Please input a new withdrawal amount of 0 or less.")
     expect(bankaccount.balance).to eq 0
   end
   it 'bank account owner should make a deposit of £50, followed by a withdrawal of £50 and add relevant transaction info to transactions array' do
-    bankaccount = BankAccount.new
     bankaccount.deposit(50)
     bankaccount.withdraw(50)
     expect(bankaccount.transactions[2]).to eq([Time.new.strftime('%d/%m/%Y'), "50", "0", "0"])
